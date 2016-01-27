d3889062545a844459c340fc0b8c6aab31cc3e20
Kevin Malkic <k_malkic@yahoo.fr>
2016-01-27 23:42:30 +0000

    Initial Commit

---
+++ MKCubeController/MKCubeController.h
@@ -0,0 +1,19 @@
+//
+//  MKCubeController.h
+//  MKCubeController
+//
+//  Created by Kevin Malkic on 27/01/2016.
+//  Copyright © 2016 Kevin Malkic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MKCubeController.
+FOUNDATION_EXPORT double MKCubeControllerVersionNumber;
+
+//! Project version string for MKCubeController.
+FOUNDATION_EXPORT const unsigned char MKCubeControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MKCubeController/PublicHeader.h>
+
+
